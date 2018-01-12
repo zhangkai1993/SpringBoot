28a0f0bb20c3042a15951afca92a4da30838cc85
Kai <kai@otocn.com>
2018-01-12 15:33:07 -0800

    init

---
+++ springboot/.metadata/.plugins/org.eclipse.core.resources/.history/a6/80bbad6548f70017115099f40d866c20
@@ -0,0 +1,7 @@
+package io.kai.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+}
---
+++ springboot/learning-api-data/src/main/java/io/kai/LearningApiDataApplication.java
@@ -0,0 +1,12 @@
+package io.kai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApiDataApplication.class, args);
+	}
+}
